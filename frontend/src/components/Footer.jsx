df30aceb7bd86217cb8c1d5bc49f59747728de19
Namrata <91418505+30Namr@users.noreply.github.com>
2025-11-20 22:15:34 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,8 +8,9 @@ const Footer = () => {
             <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
                 <div>
                     <img src={assets.logo} alt="" className='mb-5 w-32' />
-                    <p className='w-full mid:w-2/3 text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id dolorum
-                        corporis perspiciatis animi qui rem consectetur, optio fugit illum in, quidem sint corrupti consequuntur ipsa?</p>
+                    <p className='w-full mid:w-2/3 text-gray-600'>Comfort & Joy for Every Journey
+                    </p>
+                    <p className='w-full mid:w-2/3 text-gray-600'>Curated newborn essentials and pregnancy-friendly apparel crafted from soft, breathable fabrics. Thoughtfully designed for the first days and beyond.</p>
                 </div>
 
                 <div>
