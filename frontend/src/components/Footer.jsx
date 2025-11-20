60c26e053e65ef30633e6bd10819f6e58e2df9c7
Namrata <91418505+30Namr@users.noreply.github.com>
2025-11-20 22:21:32 +0530

    Update Footer component to include title
    Replaced logo image with a title in the footer component.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div>
             <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
                 <div>
-                    <img src={assets.logo} alt="" className='mb-5 w-32' />
+                   <p class="font-playfair text-3xl"> Thaai-Sei-Nalam </p>
                     <p className='w-full mid:w-2/3 text-gray-600'>Comfort & Joy for Every Journey
                     </p>
                     <p className='w-full mid:w-2/3 text-gray-600'>Curated newborn essentials and pregnancy-friendly apparel crafted from soft, breathable fabrics. Thoughtfully designed for the first days and beyond.</p>
